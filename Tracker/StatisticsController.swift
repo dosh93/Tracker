bd085e08b05deb3f750a279f0d68dcd1e10cddb3
dosh93 <l2d0sh@yandex.ru>
2024-03-10 10:38:10 +0100

    first commit

---
+++ Tracker/StatisticsController.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsController.swift
+//  Tracker
+//
+//  Created by Dosh on 09.03.2024.
+//
+
+import UIKit
+
+class StatisticsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
